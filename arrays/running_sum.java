d4d8bcf42c2b871ae7bf0ae163673c40dba75e2a
Michel Jenish J <micheljenish7708@gmail.com>
2026-07-15 23:23:31 +0530

    Implement running sum calculation in Solution class

--- arrays/running_sum.java
+++ arrays/running_sum.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int [] run_sum= new int[nums.length];
+        run_sum[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            run_sum[i]=run_sum[i-1]+nums[i];
+        }
+        return run_sum;
+    }
+}
