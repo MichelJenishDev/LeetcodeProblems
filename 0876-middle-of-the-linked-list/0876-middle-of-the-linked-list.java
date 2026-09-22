8d4f29c9f682be36ad405c21a5a069deccf2958a
Michel Jenish J <micheljenish7708@gmail.com>
2026-09-22 16:17:38 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.42%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,5 +1,14 @@
 class Solution {
-    public ListNode middleNode(ListNode head) {
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    /*public ListNode middleNode(ListNode head) {
         int n = length(head);
         ListNode current = head;
         int mid = n/2;
@@ -16,5 +25,5 @@ class Solution {
             temp = temp.next;
         }
         return count;
-    }
+    }*/
 }
